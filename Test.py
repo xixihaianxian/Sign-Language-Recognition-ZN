69176f9faf0e5f10f024333ebc3ee5df361db8b4
xixihaianxian <1668584651@qq.com>
2025-12-13 12:33:23 +0800

    2025/12/13

--- Test.py
+++ Test.py
@@ -6,6 +6,9 @@ from typing import Tuple,Dict,Any
 from torch import nn
 import Net
 import DataPreprocessing
+import os
+import VideoEnhancement
+from torch.utils.data import DataLoader
 
 # 获取配置文件
 config_params=readconfig.read_config()
@@ -13,9 +16,34 @@ config_params=readconfig.read_config()
 # 模型结构参数
 hidden_size=config_params["hiddenSize"]
 max_num_states=1
+num_workers=config_params["numWorkers"]
+pin_memory=config_params["pinMemory"]
 
+# 默认设备
+default_device=config_params["device"]
 
-def fetch_test_params()->Tuple[str,str,str,str,str,str]:
+def load_transform(is_train:bool=True):
+    r"""
+    :params:
+        is_train: 是否是测试集
+    :return:
+        transform
+    """
+    if is_train==True:
+        transform = VideoEnhancement.Compose([
+            VideoEnhancement.RandomCrop(size=224),
+            VideoEnhancement.RandomHorizontalFlip(prob=0.5),
+            VideoEnhancement.ToTensor(),
+            VideoEnhancement.TemporalRescale(temp_scaling=0.2)
+        ])
+    else:
+        transform = VideoEnhancement.Compose([
+            VideoEnhancement.CenterCrop(size=224),
+            VideoEnhancement.ToTensor(),
+        ])
+    return transform
+
+def fetch_data_params()->Tuple[str,str,str,str,str,str]:
     r"""
     :return: test_data_path,test_label_path,module_choice,data_set_name
     """
@@ -32,13 +60,22 @@ def fetch_test_params()->Tuple[str,str,str,str,str,str]:
     return test_data_path,train_label_path,valid_label_path,test_label_path,module_choice,data_set_name
 
 def data_processing(train_label_path,valid_label_path,test_label_path,data_set_name):
+    r"""
+    :params:
+        train_label_path: 训练集标签路径
+        valid_label_path: 验证集标签路径
+        test_label_path: 测试集标签路径
+        data_set_name: 数据集名称
+    :return:
+        word2idx,word_number,idx2word
+    """
     word2idx, word_number, idx2word = DataPreprocessing.word2id(train_label_path=train_label_path,
                                                                 valid_label_path=valid_label_path,
                                                                 test_label_path=test_label_path,
                                                                 data_set_name=data_set_name)
     return word2idx,word_number,idx2word
 
-def load_module(module_choice:str,word_number:int,data_set_name:str,module_path:Dict[str,Any]="module/best.pth"):
+def load_module(module_choice:str,word_number:int,data_set_name:str,device:None,module_path:str="module/best.pth"):
     r"""
     :params:
         module_choice: 模型选择
@@ -48,7 +85,39 @@ def load_module(module_choice:str,word_number:int,data_set_name:str,module_path:
     """
     module = Net.ModuleNet(hidden_size=hidden_size, word_set_num=word_number * max_num_states + 1,
                           module_choice=module_choice, data_set_name=data_set_name, is_flag=True)
-    module_dict=torch.load(module_path,map_location="cpu")
+    if not os.path.exists(module_path):
+        logger.error(f"Not exists {module_path}, please check {module_path}!")
+        exit(1)
+    module_dict:Dict[str,Any]=torch.load(module_path,map_location=default_device if device is None else device)
+    module.load_state_dict(module_dict["module_state_dict"])
+    return module
+
+def get_data_loader(data_set_name:str,data_path:str,label_path,word2idx,is_train=False,batch_size=1,shuffle:bool=True):
+    r"""
+    :params:
+        data_set_name: 数据集名称
+        data_path: 数据路径
+        label_path: 标签路径
+        word2idx: word到id的映射
+        is_train: 是否是训练
+        batch_size: 批量大小
+        shuffle: 是否打乱顺序
+    :return:
+        loader
+    """
+    transform=load_transform(is_train)
+    class_name=data_set_name.replace("-","")
+    method=getattr(DataPreprocessing,class_name)
+    test_data = method(image_dir_path=data_path, label_path=label_path, word2idx=word2idx,
+                       data_set_name=data_set_name, is_train=is_train, transform=transform)
+    test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                             pin_memory=pin_memory, collate_fn=DataPreprocessing.collate_fn, drop_last=True)
+    return test_loader
 
 def test():
     pass
+
+if __name__=="__main__":
+    test_data_path, train_label_path, valid_label_path, test_label_path, module_choice, data_set_name = fetch_data_params()
+    word2idx, word_number, idx2word=data_processing(train_label_path, valid_label_path, test_label_path, data_set_name)
+    print(word_number)
